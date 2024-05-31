35d334215a947b483f2b0baafc8459bf7e7a7926
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2024-05-31 08:38:35 +0700

    test2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Memora",
-  description: "Memora makes memmorization easy with flashcard",
+  title: "Dentistry",
+  description: "Make appointments with the best dentists in the world.",
 };
 
 export default function RootLayout({
