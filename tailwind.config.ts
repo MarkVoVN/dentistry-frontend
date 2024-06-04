dda63005772327e63ec41729a9a6bcfaad823c57
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2024-06-04 12:03:40 +0700

    added footer, with tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,123 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
-      center: true,
-      padding: "2rem",
+      // center: true,
       screens: {
-        "2xl": "1400px",
+        "2xl": "1204px",
       },
     },
+    fontFamily: {
+      "be-vietnam-pro": ["Be Vietnam Pro", "sans-serif"],
+    },
+    colors: {
+      primary: {
+        // DEFAULT: "#005669",
+        // 900: "#00718A",
+        // 800: "#0098ba",
+        // 700: "#00bae3",
+        // 600: "#0cd3ff",
+        // 500: "#35daff",
+        // 400: "#5de2ff",
+        // 300: "#85e9ff",
+        // 200: "#aef0ff",
+        // 100: "#d7f8ff",
+        // 50: "#ebfbff",
+
+        DEFAULT: "#0516cd",
+        900: "#525edc",
+        800: "#7780e4",
+        700: "#929ae9",
+        600: "#a9aeee",
+        500: "#bcc0f2",
+        400: "#cdd1f5",
+        300: "#dddff8",
+        200: "#f9f9fe",
+        100: "#ebedfb",
+        50: "#dddff8",
+      },
+      secondary: {
+        // DEFAULT: "#6CB2BC",
+        // 900: "#7bbac3",
+        // 800: "#89c1c9",
+        // 700: "#98c9d0",
+        // 600: "#a7d1d7",
+        // 500: "#b6d9de",
+        // 400: "#c4e0e4",
+        // 300: "#d3e8eb",
+        // 200: "#e2f0f2",
+        // 100: "#f0f7f8",
+        // 50: "#f8fbfc",
+
+        DEFAULT: "#f6fafd",
+        900: "#e2eff9",
+        800: "#cce4f5",
+        700: "#b5d8f0",
+        600: "#9bcaeb",
+        500: "#7dbae5",
+        400: "#5aa7de",
+        300: "#2d8fd5",
+        200: "#006fbe",
+        100: "#004170",
+      },
+      error: {
+        1: "#FEF8F6",
+        2: "#ff003c",
+      },
+      accent: {
+        1: "#d2202f",
+        2: "#df5f6a",
+        3: "#e6838b",
+      },
+      discount: "#19b43c",
+      link: "#0984e3",
+    },
     extend: {
+      boxShadow: {
+        "search-bar": "0px 2px 4px 0px #00000026",
+      },
+      backgroundImage: {
+        "gradient-132": "linear-gradient(132deg, var(--tw-gradient-stops))",
+      },
       colors: {
+        shade: {
+          "1-100%": "#FFFFFF",
+          "1-85%": "rgba(255, 255, 255, 0.85)",
+          "1-75%": "rgba(255, 255, 255, 0.75)",
+          "2-100%": "#222222",
+          "2-30%": "rgba(34, 34, 34, 0.3)",
+          "2-5%": "rgba(34, 34, 34, 0.05)",
+        },
+        neutral: {
+          1: "#F7F7F7",
+          2: "#F2F2F2",
+          3: "#DDDDDD",
+          4: "#D3D3D3",
+          5: "#C2C2C2",
+          6: "#B0B0B0",
+          7: "#636e72",
+          8: "#2d3436",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+        // primary: {
+        //   DEFAULT: "hsl(var(--primary))",
+        //   foreground: "hsl(var(--primary-foreground))",
+        // },
+        // secondary: {
+        //   DEFAULT: "hsl(var(--secondary))",
+        //   foreground: "hsl(var(--secondary-foreground))",
+        // },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
@@ -60,12 +146,12 @@ const config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
@@ -75,6 +161,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+};
