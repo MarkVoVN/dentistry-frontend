8d8a210d71afa7093e4edd23c4a3c3f230a368fb
Minh Khoi <62336255+MarkVoVN@users.noreply.github.com>
2024-07-09 16:20:56 +0700

    Fix service (#8)
    * added authen
    
    * service
    
    * fix service api and update ui

--- src/lib/api/serviceAPI.ts
+++ src/lib/api/serviceAPI.ts
@@ -52,10 +52,10 @@ export const updateService = (data: ServiceModel) => {
   console.log(data);
   return request({
     method: "PUT",
-    url: `${BASE_URL}`,
-    params: {
-      id: data.serviceID,
-    },
+
+    url: `${BASE_URL}/${data.serviceID}`,
+
+
     data,
   });
 };
